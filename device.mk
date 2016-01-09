44b8d0ecef1b22aa965cc09b6f19523815e6d3d5
Stephen Bird <sbird@cyngn.com>
2016-01-09 13:59:30 -0800

    Cleanup, fix some missing items and use opensource time
    Change-Id: I1a6cf812fc84f6d79397ee0e3f01a6662e9225f2
    
    Conflicts:
    	aicp.dependencies
    	overlay/frameworks/base/core/res/res/values/config.xml

--- device.mk
+++ device.mk
@@ -74,6 +74,11 @@ PRODUCT_PACKAGES += \
 PRODUCT_PACKAGES += \
     thermanager
 
+# Sony timekeep
+PRODUCT_PACKAGES += \
+    timekeep \
+    TimeKeep
+
 # never dexopt the MotoSignature
 $(call add-product-dex-preopt-module-config,MotoSignatureApp,disable)
 
