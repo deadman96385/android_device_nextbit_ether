f93e4bcda595f62552c3004845517c30997858cf
Griffin Millender <griffinn.millender@gmail.com>
2016-03-14 12:10:04 -0500

    Don't dexpreopt prebuilts
    Change-Id: I456d6bdcbc827eba1d42930ce8b104501c89dccf

--- device.mk
+++ device.mk
@@ -81,9 +81,6 @@ PRODUCT_PACKAGES += \
     timekeep \
     TimeKeep
 
-# never dexopt the MotoSignature
-$(call add-product-dex-preopt-module-config,MotoSignatureApp,disable)
-
 # Live Wallpapers
 PRODUCT_PACKAGES += \
     LiveWallpapersPicker \
