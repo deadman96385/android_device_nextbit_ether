b2395b3a686c1a98362c1a736b223f81edd57583
deadman96385 <seanhoyt963@gmail.com>
2016-03-24 22:53:27 -0500

    Fixup system.prop with proper values
    mainly remove ones that do not exist

--- system_prop.mk
+++ system_prop.mk
@@ -7,10 +7,7 @@ PRODUCT_PROPERTY_OVERRIDES += \
 
 # Camera
 PRODUCT_PROPERTY_OVERRIDES += \
-    ro.qc.sdk.camera.facialproc=false \
-    persist.camera.HAL3.enabled=0 \
-    persist.camera.tnr.video=on \
-    persist.camera.tnr.preview=on
+    ro.qc.sdk.camera.facialproc=false
 
 # Bluetooth
 PRODUCT_PROPERTY_OVERRIDES += \
@@ -21,9 +18,6 @@ PRODUCT_PROPERTY_OVERRIDES += \
 
 # GPS
 PRODUCT_PROPERTY_OVERRIDES += \
-    persist.mot.gps.assisted=false \
-    persist.mot.gps.conf.from.sim=true \
-    persist.mot.gps.smart_battery=1 \
     persist.gps.qc_nlp_in_use=0 \
     ro.gps.agps_provider=1 \
     ro.pip.gated=0
@@ -51,15 +45,14 @@ PRODUCT_PROPERTY_OVERRIDES += \
     persist.radio.apn_delay=5000 \
     persist.sys.qc.sub.rdump.on=1 \
     persist.qcril_uim_vcc_feature=1 \
-    persist.data.qmi.adb_logmask=0 \
-    net.tethering.noprovisioning=true \
+    persist.data.qmi.adb_logmask=0
 
 # Graphics
 PRODUCT_PROPERTY_OVERRIDES += \
     debug.sf.hw=1 \
     debug.egl.hw=1 \
     ro.opengles.version=196609 \
-    ro.sf.lcd_density=560
+    ro.sf.lcd_density=480
 
 # HWUI
 PRODUCT_PROPERTY_OVERRIDES += \
@@ -74,17 +67,6 @@ PRODUCT_PROPERTY_OVERRIDES += \
     persist.demo.hdmirotationlock=false \
     dev.pm.dyn_samplingrate=1 \
     ro.hwui.texture_cache_flushrate=0.4
-
-# USB
-PRODUCT_PROPERTY_OVERRIDES += \
-    ro.usb.mtp=0x2e82 \
-    ro.usb.mtp_adb=0x2e76 \
-    ro.usb.ptp=0x2e83 \
-    ro.usb.ptp_adb=0x2e84 \
-    ro.usb.bpt=0x2ee5 \
-    ro.usb.bpt_adb=0x2ee6 \
-    ro.usb.bpteth=0x2ee7 \
-    ro.usb.bpteth_adb=0x2ee8
 
 # Vendor specific
 PRODUCT_PROPERTY_OVERRIDES += \
@@ -102,7 +84,6 @@ PRODUCT_PROPERTY_OVERRIDES += \
     ro.hdmi.enable=true \
     ro.qc.sdk.audio.ssr=false \
     ro.qc.sdk.audio.fluencetype=none \
-    ro.fm.transmitter=false \
     ro.audio.monitorRotation=true \
     media.stagefright.enable-player=true \
     media.stagefright.enable-http=true \
@@ -111,41 +92,19 @@ PRODUCT_PROPERTY_OVERRIDES += \
     media.stagefright.enable-fma2dp=true \
     media.stagefright.enable-scan=true \
     media.aac_51_output_enabled=true \
-    persist.dpm.feature=3 \
-    persist.audio.calfile0=/etc/acdbdata/Bluetooth_cal.acdb \
-    persist.audio.calfile1=/etc/acdbdata/General_cal.acdb \
-    persist.audio.calfile2=/etc/acdbdata/Global_cal.acdb \
-    persist.audio.calfile3=/etc/acdbdata/Handset_cal.acdb \
-    persist.audio.calfile4=/etc/acdbdata/Hdmi_cal.acdb \
-    persist.audio.calfile5=/etc/acdbdata/Headset_cal.acdb \
-    persist.audio.calfile6=/etc/acdbdata/Speaker_cal.acdb \
-    persist.audio.dualmic.config=endfire \
-    persist.audio.fluence.voicecall=true \
-    persist.audio.fluence.voicecomm=true \
-    persist.audio.fluence.voicerec=false \
-    persist.audio.fluence.speaker=false \
-    audio.offload.disable=0 \
-    audio.offload.buffer.size.kb=32 \
-    audio.offload.multiple.enabled=true \
-    audio.offload.pcm.16bit.enable=false \
-    audio.offload.pcm.24bit.enable=false \
-    audio.offload.gapless.enabled=false \
-    av.offload.enable=false \
     mmp.enable.3g2=true \
-    vidc.enc.dcvs.extra-buff-count=2 \
-    tunnel.audio.encode=false \
-    use.voice.path.for.pcm.voip=true \
+    mm.enable.smoothstreaming=true \
+    media.aac_51_output_enabled=true
 
 # Misc
 PRODUCT_PROPERTY_OVERRIDES += \
     ro.qc.sdk.sensors.gestures=true \
     ro.qc.sdk.gestures.camera=false \
-    ro.qualcomm.cabl=0 \
+    ro.qc.sdk.camera.facialproc=false
+    ro.qualcomm.cabl=2 \
     persist.sys.wfd.virtual=0 \
     persist.debug.coresight.config=stm-events \
-    ro.bug2go.magickeys=24,26 \
-    drm.service.enabled=true \
-    mdc_initial_max_retry=10
+    drm.service.enabled=true
 
 # Tethering
 PRODUCT_PROPERTY_OVERRIDES += \
@@ -154,19 +113,3 @@ PRODUCT_PROPERTY_OVERRIDES += \
 # FRP
 PRODUCT_PROPERTY_OVERRIDES += \
     ro.frp.pst=/dev/block/bootdevice/by-name/frp
-
-# SSR
-PRODUCT_PROPERTY_OVERRIDES += \
-    persist.sys.ssr.restart_level=ALL_ENABLE
-
-# OTHER - pulled out of ramdisk
-PRODUCT_PROPERTY_OVERRIDES += \
-    net.tcp.2g_init_rwnd=10 \
-    net.tcp.delack.default=1 \
-    net.tcp.delack.wifi=20 \
-    net.tcp.delack.lte=1 \
-    net.tcp.usercfg.default=0 \
-    net.tcp.usercfg.wifi=1 \
-    net.tcp.usercfg.lte=0 \
-    ro.telephony.call_ring.multiple=false \
-    wifi.interface=wlan0
