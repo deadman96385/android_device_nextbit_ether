944fedbf6ba3d921485e742c29652334607aa138
deadman96385 <seanhoyt963@gmail.com>
2016-03-25 19:05:44 -0600

    Add fingerprint and desc

--- cm.mk
+++ cm.mk
@@ -25,3 +25,7 @@ PRODUCT_DEVICE := ether
 PRODUCT_BRAND := Nextbit
 PRODUCT_MODEL := Robin
 PRODUCT_MANUFACTURER := Nextbit
+
+PRODUCT_BUILD_PROP_OVERRIDES += \
+     BUILD_FINGERPRINT=Nextbit/ether/ether:6.0/MRA58K/00WW_1_19E:user/release-keys \
+     PRIVATE_BUILD_DESC="ether-user 6.0 MRA58K 00WW_1_19E release-keys"
